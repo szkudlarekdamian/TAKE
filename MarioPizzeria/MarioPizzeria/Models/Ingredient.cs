c241a82f1e42a5f45895ecee24eaa9fa6aed5ed4
Damian <Damian@DESKTOP-FABPVSJ>
2020-06-22 19:15:52 +0200

    Entities created

---
+++ MarioPizzeria/MarioPizzeria/Models/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarioPizzeria.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public virtual ICollection<Pizza> PizzasOn { get; set; }
+    }
+}
---
+++ MarioPizzeria/MarioPizzeria/Models/Pizza.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarioPizzeria.Models
+{
+    public class Pizza
+    {
+        public Pizza()
+        {
+            var list = new[]
+            {
+                new { Crust = "Cienkie", Value = "Cienkie" },
+                new { Crust = "Grube", Value = "Grube" }
+            }.ToList();
+            CrustList = new SelectList(list, "Crust", "Value", "");
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public double PriceS { get; set; }
+
+        [Required]
+        public double PriceM { get; set; }
+
+        [Required]
+        public double PriceL { get; set; }
+
+        public SelectList CrustList { get; set; }
+        [Required]
+        public string Crust { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
